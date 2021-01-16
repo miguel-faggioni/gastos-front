2928f56934fae14befb72a8f07c95858f57a7080
Miguel <miguel@ganesha>
2021-01-16 14:12:09 -0300

    added handlers to erase and download all data

--- src/components/Configurations.vue
+++ src/components/Configurations.vue
@@ -1,12 +1,26 @@
 <template>
   <v-container class="d-flex flex-column align-start min-height">
-    <v-btn v-on:click="downloadData()" outlined class="mb-8 mt-2 full-width" color="primary">
+    <v-btn
+      v-on:click="downloadData()"
+      :loading="loading.downloadData"
+      :disabled="loading.downloadData"
+      outlined
+      class="mb-8 mt-2 full-width"
+      color="primary"
+    >
       <v-icon left>
         mdi-cash-multiple
       </v-icon>
       Baixar gastos
     </v-btn>
-    <v-btn v-on:click="eraseData()" outlined class="mb-5 full-width" color="error">
+    <v-btn
+      v-on:click="eraseData()"
+      :loading="loading.eraseData"
+      :disabled="loading.eraseData"
+      outlined
+      class="mb-5 full-width"
+      color="error"
+    >
       <v-icon left>
         mdi-cash-remove
       </v-icon>
@@ -20,17 +34,10 @@
     >
       Logout
     </v-btn>
-    <v-snackbar
-      v-model="snackbar.show"
-      :timeout="snackbar.timeout"
-    >
+    <v-snackbar v-model="snackbar.show" :timeout="snackbar.timeout">
       {{ snackbar.msg }}
       <template v-slot:action="{ attrs }">
-        <v-btn
-          text
-          v-bind="attrs"
-          @click="snackbar.show = false"
-        >
+        <v-btn text v-bind="attrs" @click="snackbar.show = false">
           OK
         </v-btn>
       </template>
@@ -44,8 +51,12 @@
       snackbar: {
         msg: 'Erro ao realizar logout',
         show: false,
-        timeout: 2000
-      }
+        timeout: 2000,
+      },
+      loading: {
+        downloadData: false,
+        eraseData: false,
+      },
     }),
     methods: {
       logout: function() {
@@ -53,8 +64,29 @@
         this.$router.push({
           name: 'Login',
         })
-      }
-    }
+      },
+      downloadData: async function() {
+        console.log('ue')
+        this.loading.downloadData = true
+        try {
+          await this.$store.dispatch('gasto/downloadAll', { format: 'csv' })
+        } catch (err) {
+          this.snackbar.msg = 'Erro ao tentar baixar gastos'
+          this.snackbar.show = true
+        }
+        this.loading.downloadData = false
+      },
+      eraseData: async function() {
+        this.loading.eraseData = true
+        try {
+          await this.$store.dispatch('gasto/deleteAll')
+        } catch (err) {
+          this.snackbar.msg = 'Erro ao tentar apagar gastos'
+          this.snackbar.show = true
+        }
+        this.loading.eraseData = false
+      },
+    },
   }
 </script>
 
--- src/store/gasto.api.js
+++ src/store/gasto.api.js
@@ -15,16 +15,24 @@ export default {
     // asynchronous
     async get(context) {
       let response = await axios.get('/gastos')
-      context.commit('set',response.data)
+      context.commit('set', response.data)
     },
-    async create(context, { valor, data, categoria, pagamento }){
-      return axios.post('/gastos',{
+    async create(context, { valor, data, categoria, pagamento }) {
+      return axios.post('/gastos', {
         valor: valor,
         data: data,
         idCategoria: categoria.id,
         idModoDePagamento: pagamento.id,
       })
-    }
+    },
+    async deleteAll(context) {
+      return axios.delete('/gastos').then(() => {
+        context.commit('set', [])
+      })
+    },
+    async downloadAll(context, { format }) {
+      return axios.get(`/gastos/download/${format}`)
+    },
   },
   modules: {},
   getters: {},
