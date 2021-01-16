be750d0e42472fbbda14598eb8ed711bcf40a107
Miguel <miguel@ganesha>
2021-01-16 11:16:40 -0300

    added light-green theme to app

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+// console.log(colors)
+
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.lightGreen.darken1,
+        secondary: colors.lightGreen.lighten1,
+        accent: colors.green.base,
+      }
+    }
+  }
+})
